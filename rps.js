6023bb857537ce35d45f0c10f6017f8efbaea702
suryansi dhal <suryansidhal49@gmail.com>
2026-02-14 21:54:54 +0530

    Add files via upload
    This project is an interactive Rock Paper Scissors game where the player competes against the computer in a fun and engaging way. The game allows users to select their move by clicking on intuitive icons, making gameplay simple and user-friendly. The computer generates a random move for each round, and the result is displayed instantly as a win, loss, or draw. Both the player’s score and the computer’s score are tracked and updated in real time. This project demonstrates basic game logic, event handling, and dynamic UI updates, making it a great beginner-friendly project for learning core programming concepts.

---
+++ rps.js
@@ -0,0 +1,7 @@
+let userscore=0;let compscore=0;const choices=document.querySelectorAll(".choice");const msg=document.querySelector("#msg");const userscorepara=document.querySelector("#user-score");const compscorepara=document.querySelector("#comp-score");const gencomputerchoice=()=>{const options=["rock","paper","scissors"];const comp=Math.floor(Math.random()*3);return options[comp]}
+const drawgame=()=>{msg.innerText="game draw!";msg.style.backgroundColor="white"}
+const showwiner=(userwin,userchoice,compchoice)=>{if(userwin){userscore++;userscorepara.innerText=userscore;msg.innerText=`user won! ${userchoice} beats ${compchoice}`;msg.style.backgroundColor="green"}else{compscore++
+compscorepara.innerText=compscore;msg.innerText=`user lost! ${compchoice} beats ${userchoice}`;msg.style.backgroundColor="red"}}
+const playgame=(userchoice)=>{console.log("user choice=",userchoice);const compchoice=gencomputerchoice();console.log("computer choice=",compchoice);if(userchoice===compchoice){drawgame()}else{let userwin=!0;if(userchoice==="rock"){userwin=compchoice==="paper"?false:!0}else if(userchoice==="paper"){userwin=compchoice==="scissors"?false:!0}else{userwin=compchoice==="rock"?false:!0}
+showwiner(userwin,userchoice,compchoice)}}
+choices.forEach((choice)=>{choice.addEventListener("click",()=>{const userchoice=choice.getAttribute("id");playgame(userchoice)})})
